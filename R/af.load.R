1778cfe8abf52bd5a94ffa6139df947023bac99e
Mark Daku <mark.daku@gmail.com>
2019-07-09 16:42:40 +0200

    Fixed redundancy and global af.round

--- R/af.load.R
+++ R/af.load.R
@@ -12,32 +12,32 @@ af.load <- function(round=6){
   if (round == 6){
     # 2016
     url <- "http://afrobarometer.org/sites/default/files/data/round-6/merged_r6_data_2016_36countries2.sav"
-    af.round <- 6
+    af.round <<- 6
   }
   else if (round == 5){
     url <- "http://afrobarometer.org/sites/default/files/data/round-5/merged-round-5-data-34-countries-2011-2013-last-update-july-2015.sav"
     warning("Afrobarometer round 5 is not the most recent round of data")
-    af.round <- 5
+    af.round <<- 5
   }
   else if (round ==4){
     url <- "http://afrobarometer.org/sites/default/files/data/round-4/merged_r4_data.sav"
     warning("Afrobarometer round 4 is not the most recent round of data")
-    af.round <- 4
+    af.round <<- 4
   }
   else if (round ==3){
     url <- "http://afrobarometer.org/sites/default/files/data/round-3/merged_r3_data.sav"
     warning("Afrobarometer round 3 is not the most recent round of data")
-    af.round <- 3
+    af.round <<- 3
   }
   else if (round ==2){
     url <- "http://afrobarometer.org/sites/default/files/data/round-2/merged_r2_data.sav"
     warning("Afrobarometer round 2 is not the most recent round of data")
-    af.round <- 2
+    af.round <<- 2
   }
   else if (round ==1){
     url <- "http://afrobarometer.org/sites/default/files/data/round-1/merged_r1_data.sav"
     warning("Afrobarometer round 1 is not the most recent round of data")
-    af.round <- 1
+    af.round <<- 1
   }
   else{
     stop("Only Afrobarometer rounds 1-6 are currently available")
@@ -46,6 +46,6 @@ af.load <- function(round=6){
   # Download the file & load it as an SPSS file
   temp <- tempfile()
   download.file(url, temp)
-  afro.src <<- read_sav(temp)
-  afro.src <<- afro.clean(afro.src)
+  afro <<- read_sav(temp)
+  afro <<- afro.clean(afro.src)
 }
