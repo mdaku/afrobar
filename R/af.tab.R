33d67525a83ec9b4de84a40c9854eeab06d363b1
Mark Daku <mark.daku@gmail.com>
2019-07-10 16:18:52 +0200

    Fix messed up merge

--- R/af.tab.R
+++ R/af.tab.R
@@ -24,7 +24,7 @@ af.tab <- function(question, question2=NULL, digits=2, miss=T, pos="dodge"){
       ggtitle(label=var_label(afro[question][1])[[1]], subtitle = paste("Source: Afrobarometer, Round", af.round)) +
       geom_bar()
 
-<<<<<<< HEAD
+
       title<-var_label(afro[question][1])[[1]]
 
       print(g)
@@ -37,13 +37,13 @@ af.tab <- function(question, question2=NULL, digits=2, miss=T, pos="dodge"){
       cat("\n")
       cat("Raw numbers\n")
       summary(a[[question]])
-=======
+
       print(g)
       print(var_label(afro[question][1])[[1]])
       a[[question]] <- factor(a[[question]])
       round(prop.table(table(a[question])),digits)
 
->>>>>>> 5213a1ca1e85bcce507cf629acddab24fe7cf2d3
+
   }
 
   else{
@@ -62,27 +62,20 @@ af.tab <- function(question, question2=NULL, digits=2, miss=T, pos="dodge"){
         filter(eval(parse(text=question2)) != "Refused")
     }
 
-<<<<<<< HEAD
+
     title <- paste(var_label(afro[question][1])[[1]],var_label(afro[question2][1])[[1]], sep=" by \n")
-=======
-    t <- paste(var_label(afro[question][1])[[1]],var_label(afro[question2][1])[[1]], sep=" by \n")
->>>>>>> 5213a1ca1e85bcce507cf629acddab24fe7cf2d3
     g <- a %>%
       #ggplot(aes(x=eval(parse(text=question)), y=eval(parse(text=question2)))) +
       ggplot(aes(x=eval(parse(text=question)))) +
       theme_minimal() +
       xlab("")+
       ylab("Count") +
-<<<<<<< HEAD
+
       ggtitle(label=title, subtitle = paste("Source: Afrobarometer, Round", af.round)) +
-=======
-      ggtitle(label=t, subtitle = paste("Source: Afrobarometer, Round", af.round)) +
->>>>>>> 5213a1ca1e85bcce507cf629acddab24fe7cf2d3
       labs(fill="") +
       geom_bar(aes(fill=eval(parse(text=question2))), position=pos)
 
     print(g)
-<<<<<<< HEAD
 
     # Perform some tests here
     chi <- chisq.test(afro[[question]], afro[[question2]])
@@ -107,8 +100,8 @@ af.tab <- function(question, question2=NULL, digits=2, miss=T, pos="dodge"){
     cat(question2)
     cat(":\n")
     print(summary(a[[question2]]))
-    }
-=======
+
+
     a[[question]] <- factor(a[[question]])
     a[[question2]] <- factor(a[[question2]])
 
@@ -116,7 +109,7 @@ af.tab <- function(question, question2=NULL, digits=2, miss=T, pos="dodge"){
 
     round(prop.table(table(a[[question]], a[[question2]])),digits)
   }
->>>>>>> 5213a1ca1e85bcce507cf629acddab24fe7cf2d3
+
 
 
 }
