5213a1ca1e85bcce507cf629acddab24fe7cf2d3
Mark Daku <mark.daku@gmail.com>
2019-07-10 11:56:14 +0200

    Added more complexity to tables

--- R/af.tab.R
+++ R/af.tab.R
@@ -1,30 +1,71 @@
 # Descriptive Table & Graphs
 library(ggplot2)
-
-af.tab <- function(question, digits=2, missing=T){
+library(labelled)
+af.tab <- function(question, question2=NULL, digits=2, miss=T, pos="dodge"){
   # Need to check if question actually exists
   #v <- afro[question,]
   # We are missing the actual question text from the codebook
-  print(var_label(afro[question][1])[[1]])
-  round(prop.table(table(afro[question])),digits)
-  #eval(parse(text = "x"))
-  if(missing){
-    a <- afro
+
+  if (is.null(question2)){
+    #eval(parse(text = "x"))
+    if(miss){
+      a <- afro
+    }
+    else{
+      a <- afro %>% filter(eval(parse(text=question)) != "Missing") %>%
+      filter(eval(parse(text=question)) != "Don't know") %>%
+        filter(eval(parse(text=question)) != "Refused")
+    }
+    g <- a %>%
+      ggplot(aes(x=eval(parse(text=question)))) +
+      theme_minimal() +
+      xlab("")+
+      ylab("Count") +
+      ggtitle(label=var_label(afro[question][1])[[1]], subtitle = paste("Source: Afrobarometer, Round", af.round)) +
+      geom_bar()
+
+      print(g)
+      print(var_label(afro[question][1])[[1]])
+      a[[question]] <- factor(a[[question]])
+      round(prop.table(table(a[question])),digits)
+
   }
+
   else{
-    a <- afro %>% filter(eval(parse(text=question)) != "NA") %>%
-    filter(eval(parse(text=question)) != "Don't know")
 
+    if(miss){
+      a <- afro
+    }
+    else{
+      a <- afro %>% filter(eval(parse(text=question)) != "Missing") %>%
+        filter(eval(parse(text=question)) != "Don't know") %>%
+        filter(eval(parse(text=question)) != "Don’t know") %>%
+        filter(eval(parse(text=question)) != "Refused") %>%
+        filter(eval(parse(text=question2)) != "Missing") %>%
+        filter(eval(parse(text=question2)) != "Don't know") %>%
+        filter(eval(parse(text=question2)) != "Don’t know") %>%
+        filter(eval(parse(text=question2)) != "Refused")
+    }
+
+    t <- paste(var_label(afro[question][1])[[1]],var_label(afro[question2][1])[[1]], sep=" by \n")
+    g <- a %>%
+      #ggplot(aes(x=eval(parse(text=question)), y=eval(parse(text=question2)))) +
+      ggplot(aes(x=eval(parse(text=question)))) +
+      theme_minimal() +
+      xlab("")+
+      ylab("Count") +
+      ggtitle(label=t, subtitle = paste("Source: Afrobarometer, Round", af.round)) +
+      labs(fill="") +
+      geom_bar(aes(fill=eval(parse(text=question2))), position=pos)
+
+    print(g)
+    a[[question]] <- factor(a[[question]])
+    a[[question2]] <- factor(a[[question2]])
+
+    print(t)
+
+    round(prop.table(table(a[[question]], a[[question2]])),digits)
   }
-  a %>%
-    ggplot(aes(x=eval(parse(text=question)))) +
-    theme_minimal() +
-    xlab("")+
-    ylab("Count") +
-    ggtitle(label=var_label(afro[question][1])[[1]], subtitle = paste("Source: Afrobarometer, Round", af.round)) +
-    geom_bar()
 
-  #
-  # We also want this to actually look good
-  #question
+
 }
