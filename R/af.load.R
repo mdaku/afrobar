af0d520cd4137279b0c299923c4a8532a5c33f59
Mark Daku <mark.daku@gmail.com>
2019-07-10 16:12:21 +0200

    Add capability for local (cached) loading

--- R/af.load.R
+++ R/af.load.R
@@ -7,35 +7,42 @@ library(tidyverse)
 library(haven)
 
 # Functions
-af.load <- function(round=6){
+af.load <- function(round=6, force=FALSE){
+  # Specifies the round and whether or not to force the download
   # This may be complicated, as the connection is usually weak
   if (round == 6){
     # 2016
     url <- "http://afrobarometer.org/sites/default/files/data/round-6/merged_r6_data_2016_36countries2.sav"
+    fname <- "merged_r6_data_2016_36countries2.sav"
     af.round <<- 6
   }
   else if (round == 5){
     url <- "http://afrobarometer.org/sites/default/files/data/round-5/merged-round-5-data-34-countries-2011-2013-last-update-july-2015.sav"
+    fname <- "merged-round-5-data-34-countries-2011-2013-last-update-july-2015.sav"
     warning("Afrobarometer round 5 is not the most recent round of data")
     af.round <<- 5
   }
   else if (round ==4){
     url <- "http://afrobarometer.org/sites/default/files/data/round-4/merged_r4_data.sav"
+    fname <- "round-4/merged_r4_data.sav"
     warning("Afrobarometer round 4 is not the most recent round of data")
     af.round <<- 4
   }
   else if (round ==3){
     url <- "http://afrobarometer.org/sites/default/files/data/round-3/merged_r3_data.sav"
+    fname <- "merged_r3_data.sav"
     warning("Afrobarometer round 3 is not the most recent round of data")
     af.round <<- 3
   }
   else if (round ==2){
     url <- "http://afrobarometer.org/sites/default/files/data/round-2/merged_r2_data.sav"
+    fname <- "merged_r2_data.sav"
     warning("Afrobarometer round 2 is not the most recent round of data")
     af.round <<- 2
   }
   else if (round ==1){
     url <- "http://afrobarometer.org/sites/default/files/data/round-1/merged_r1_data.sav"
+    fname <- "merged_r1_data.sav"
     warning("Afrobarometer round 1 is not the most recent round of data")
     af.round <<- 1
   }
@@ -44,8 +51,20 @@ af.load <- function(round=6){
   }
 
   # Download the file & load it as an SPSS file
-  temp <- tempfile()
-  download.file(url, temp)
-  afro <<- read_sav(temp)
-  afro <<- afro.clean(afro.src)
+
+  # First, check to see if the file exists locally
+  if (file.exists(fname) & force==FALSE){
+    # Does the file exist?
+    message("Loading local file")
+    afro <<- read_sav(fname)
+  }
+  else{
+    # Downloading file from Afrobarometer.org
+    message("Downloading file from Afrobarometer.org")
+    temp <- tempfile()
+    download.file(url, temp)
+    afro <<- read_sav(temp)
+  }
+  message("Cleaning source file")
+  afro <<- af.clean(afro)
 }
