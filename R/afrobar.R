5c049c43e83b1192289b7e42c895076017efa09b
Mark Daku <mark.daku@gmail.com>
2019-07-10 17:13:03 +0200

    Add package documentation

---
+++ R/afrobar.R
@@ -0,0 +1,14 @@
+#' afrobar: A package for computating the notorious bar statistic.
+#'
+#' The afrobar package provides functions for the simple loading and exploration of afrobarometer data
+#'
+#' @section Loading & Preparing Data
+#' af.load()
+#' af.clean()
+#'
+#' @section Exploring Data
+#' af.tab()
+#'
+#' @docType package
+#' @name afrobar
+NULL
