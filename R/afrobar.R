2af9f5dcc470e3b4ed6fbb2563ef21ca17ea67fe
Mark Daku <mark.daku@gmail.com>
2019-07-12 14:01:47 +0200

    Update naming convention

--- R/afrobar.R
+++ R/afrobar.R
@@ -3,11 +3,11 @@
 #' The afrobar package provides functions for the simple loading and exploration of afrobarometer data
 #'
 #' @section Loading and Preparing Data
-#' af.load()
-#' af.clean()
+#' af_load()
+#' af_clean()
 #'
 #' @section Exploring Data
-#' af.tab()
+#' af_tab()
 #'
 #' @docType package
 #' @name afrobar
