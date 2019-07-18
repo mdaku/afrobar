fa35f9c0cb494cc8b31315a560be554db039479b
Mark Daku <mark.daku@gmail.com>
2019-07-18 16:56:07 +0200

    Cleared out refused / missing values, but left in "Don't Knows" - handled inconsistently across rounds

--- R/af_clean.R
+++ R/af_clean.R
@@ -1,6 +1,8 @@
 #' Cleans the Afrobarometer dataset
 #'
 #' Returns a clean version of the Afrobarometer dataset. Normally called by default by \code{af.load()}
+#' Important: For all rounds of the Afrobarometer, this function recodes missing, refused, etc.
+#' data as NA.  If you would prefer to keep the data unmodified, you must run af_load(clean=FALSE)
 #'
 #' @name af_clean
 #' @param afro A dataframe containing Afrobarometer data
@@ -17,10 +19,28 @@ af_clean <- function(afro){
   afro <- afro %>% rename_all(tolower)
 
   if (af_round == 1){
-    # Replace 99 with NA
+    # Round 1
+    # 9 = Don't Know - Leave in
+    # 97 = Not applicable
+    # 98 = Refused
+    # 99 = Missing Data
 
-    afro <- afro %>% mutate_all(~ replace(., . == 99 | . == "99", NA))
-
+    afro <- afro %>%
+      mutate_all(~ replace(., . == "97" | . == "98" | . == "99" | . == "999" | . == "998" | . == "997" | . == "996", NA)) %>%
+      mutate_if(., is.labelled, as_factor)
+  }
+  else if (af_round ==2 | af_round == 3 | af_round ==4 | af_round ==5 | af_round ==6){
+    # Round 2-6
+    # -1 = Missing Data
+    # We HAVE to leave the Don't Knows alone
+    # 995 Other
+    # 996 No Further Answer
+    # 997 Not Applicable
+    # 998 Refused to Answer
+    # 999 Don't Know / 9999
+    afro <- afro %>%
+      mutate_all(~ replace(., . == "-1" | . == "995" | . == "996" | . == "997" | . == "998" | . == "98" | . == "9995" | . == "9996" | . == "9997" | . == "9998" | . == "98", NA)) %>%
+      mutate_if(., is.labelled, as_factor)
   }  # This data seems to be pretty clean already
     # Return dataframe
   afro
